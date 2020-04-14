fead83de5dde99927cbb39004632b22af778781b
Robert Kalescky <rkalescky@smu.edu>
2020-04-14 12:12:03 -0500

    OpenMP and OpenACC additions.

---
+++ workshops/openacc_tutorial/01_saxpy/saxpy_openacc.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+
+void saxpy(int n, float a, float *x, float *restrict y) {
+   #pragma acc parallel loop
+   for (int i = 0; i < n; ++i)
+      y[i] = a * x[i] + y[i];
+}
+
+int main(int argc, char **argv) {
+   int N = 1<<20; // 1 million floats
+   if (argc > 1) N = atoi(argv[1]);
+   float *x = (float*)malloc(N * sizeof(float));
+   float *y = (float*)malloc(N * sizeof(float));
+   for (int i = 0; i < N; ++i) {
+      x[i] = 2.0f;
+      y[i] = 1.0f;
+   }
+   saxpy(N, 3.0f, x, y);
+   return 0;
+}
